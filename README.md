4700b98c03bc384af7c0cebcaa6bb732c44e81e9
Alex Hunter <almrdog@gmail.com>
2021-12-23 21:21:36 +0000

    add read me;

---
+++ README.md
@@ -0,0 +1,26 @@
+example of use
+```javascript
+  const principal = 1000;
+  const interestRate = 0.05;
+  const inflationRate = 0.02;
+  const numOfTimes = 10;
+
+  const futures = new InterestCalculator(
+    principal,
+    interestRate,
+    inflationRate,
+    numOfTimes
+  );
+
+  const compoundInterestDecimal = futures.getCompoundInterestDecimal();
+  const compoundInterestPercent = futures.getCompoundInterestPercent();
+  const compoundRealInterestDecimal = futures.getRealCompoundInterestDecimal();
+  const compoundRealInterestPercent = futures.getRealCompoundInterestPercent();
+
+  console.log(
+    compoundInterestDecimal,
+    compoundInterestPercent,
+    compoundRealInterestDecimal,
+    compoundRealInterestPercent,
+  );
+```
--- interestCalcClass.js
+++ interestCalcClass.js
@@ -1,4 +1,4 @@
-class InterestCalculator {
+export default class InterestCalculator {
   constructor(principal, interestRate, inflationRate, times) {
     this.principal = principal;
     this.interestRate = interestRate;
@@ -14,17 +14,19 @@ class InterestCalculator {
   simpleInterestPercent(amount, rate) {
     return (amount * rate) / 100;
   }
-  realInterestDecimal(amount) {
-    return this.simpleInterestDecimal(
-      amount,
-      this.realInterestRate(this.interesRate, this.inflationRate)
-    );
+  realInterestDecimal(amount, rate, inflationRate) {
+    function simpleInterestDecimal(amount, rate) {
+      return amount * rate;
+    }
+    const realInterestRate = rate - inflationRate;
+    return simpleInterestDecimal(amount, realInterestRate);
   }
-  realInterestPercent(amount) {
-    return this.simpleInterestDecimal(
-      amount,
-      this.realInterestRate(this.interestRate, this.inflationRate)
-    );
+  realInterestPercent(amount, rate, inflationRate) {
+    function simpleInterestPercent(amount, rate) {
+      return (amount * rate) / 100;
+    }
+    const realInterestRate = rate - inflationRate;
+    return simpleInterestPercent(amount, realInterestRate);
   }
 
   getSimpleInterest(principal, interest, frequency) {
@@ -41,28 +43,86 @@ class InterestCalculator {
     for (let i = 0; i < frequency; i++) {
       principal += interest;
       futures = [...futures, principal.toFixed(2)];
-      interest = callback(principal, this.interestRate);
+      interest = callback(principal, this.interestRate, this.inflationRate);
     }
     return futures;
   }
 
+  // getNominalInterestDecimal() {
+  //   const interest = this.simpleInterestDecimal(
+  //     this.principal,
+  //     this.interestRate
+  //   );
+  //   return this.getCompoundInterest(this.principal, interest, this.times);
+  // }
+  // getNominalInterestPercent() {
+  //   const interest = this.simpleInterestPercent(
+  //     this.principal,
+  //     this.interestRate
+  //   );
+  //   return this.getCompoundInterest(this.principal, interest, this.times);
+  // }
+  getRealInterestDecimal() {
+    const interest = this.simpleInterestDecimal(
+      this.principal,
+      this.realInterestRate(this.interestRate, this.inflationRate)
+    );
+    return this.getCompoundInterest(this.principal, interest, this.times);
+  }
+  getRealInterestPercent() {
+    const interest = this.simpleInterestPercent(
+      this.principal,
+      this.realInterestRate(this.interestRate, this.inflationRate)
+    );
+    return this.getCompoundInterest(this.principal, interest, this.times);
+  }
   getCompoundInterestDecimal() {
-    const interest = this.simpleInterestDecimal(this.principal, this.interestRate)
-    return this.getCompoundInterest(this.principal, interest, this.times, this.simpleInterestDecimal)
+    const interest = this.simpleInterestDecimal(
+      this.principal,
+      this.interestRate
+    );
+    return this.getCompoundInterest(
+      this.principal,
+      interest,
+      this.times,
+      this.simpleInterestDecimal
+    );
   }
   getCompoundInterestPercent() {
-    const interest = this.simpleInterestPercent(this.principal, this.interestRate)
-    return this.getCompoundInterest(this.principal, interest, this.times, this.simpleInterestPercent)
+    const interest = this.simpleInterestPercent(
+      this.principal,
+      this.interestRate
+    );
+    return this.getCompoundInterest(
+      this.principal,
+      interest,
+      this.times,
+      this.simpleInterestPercent
+    );
   }
   getRealCompoundInterestDecimal() {
-    const interest = this.simpleInterestDecimal(this.principal, this.realInterestRate(this.interestRate, this.inflationRate))
-    return this.getCompoundInterest(this.principal, interest, this.times, this.realInterestDecimal)
+    const interest = this.simpleInterestDecimal(
+      this.principal,
+      this.realInterestRate(this.interestRate, this.inflationRate)
+    );
+    
+    return this.getCompoundInterest(
+      this.principal,
+      interest,
+      this.times,
+      this.realInterestDecimal
+    );
   }
   getRealCompoundInterestPercent() {
-    const interest = this.simpleInterestPercent(this.principal, this.realInterestRate(this.interestRate, this.inflationRate))
-    return this.getCompoundInterest(this.principal, interest, this.times, this.realInterestDecimal)
+    const interest = this.simpleInterestPercent(
+      this.principal,
+      this.realInterestRate(this.interestRate, this.inflationRate)
+    );
+    return this.getCompoundInterest(
+      this.principal,
+      interest,
+      this.times,
+      this.realInterestPercent
+    );
   }
-  
 }
-
- module.exports.InterestCalculator = InterestCalculator
